46087b6f1b874a98647a0b02818f2afc8e41dcff
Felipe Souto Campelo <felipesoutocampelo@gmail.com>
2023-05-29 14:32:06 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,9 +25,9 @@
 
 CONFIG_DIR="/tmp/config.yml"
 
-printf "%s\n" "${@}"
+# printf "%s\n" "${@}"
 	# BASE64
-printf "Running embulk with base64 config, decoding string to %s\n" "${CONFIG_DIR}"
+# printf "Running embulk with base64 config, decoding string to %s\n" "${CONFIG_DIR}"
 echo "${BASE64_CONFIG}" | base64 -d > "${CONFIG_DIR}"
 
 /opt/embulk/embulk run "${CONFIG_DIR}"
